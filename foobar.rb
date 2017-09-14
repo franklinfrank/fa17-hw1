fbe4a610aa485d1b0040bf4bdae60710b6b02970
Jeffrey Liu <jeffreyyijie@gmail.com>
2017-09-13 20:06:14 -0700

    Updated to reflect instructions typo

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-    fin_arr = a.map{|x| x.to_i + 2}.reject!{|x| x % 2 != 0 }.uniq.reject!{|x| x > 10}
+    fin_arr = a.map{|x| x.to_i + 2}.reject!{|x| x % 2 != 0 }.uniq.reject!{|x| x >= 10}
     return fin_arr.inject(0){|sum, x| sum + x}
 
   end
