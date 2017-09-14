78a3724470f7105a5fa2fda1a8d5097a1df17981
Jeffrey Liu <jeffreyyijie@gmail.com>
2017-09-13 19:56:59 -0700

    added solutions

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,9 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    fin_arr = a.map{|x| x.to_i + 2}.reject!{|x| x % 2 != 0 }.uniq.reject!{|x| x > 10}
+    return fin_arr.inject(0){|sum, x| sum + x}
+
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,15 @@
-def squared_sum(a, b)
-  # Q1 CODE HERE
+def squared_sum(a,b)
+	return (a+b)**2
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+	return a.map{|x| x + 1}.sort
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+	return first_name + " " + last_name
 
 end
 
